47ddf08f353e388a3d76b499b43313c839f32a00
devopsprasanna919 <devopsprasanna919@gmail.com>
2025-07-24 00:02:40 +0530

    Update Calculator.groovy

--- src/com/academy/build/Calculator.groovy
+++ src/com/academy/build/Calculator.groovy
@@ -1,11 +1,10 @@
 pacakge com.academy.build;
-
 class Calculator {
     def jenkins
 Calculator(jenkins) {
     this.jenkins = jenkins
 }
-def addition(firstNumber, secondNumber) {
+def add(firstNumber, secondNumber) {
     return firstNumber + secondNumber
 }
 }
