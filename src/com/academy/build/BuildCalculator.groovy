aa65a407d3ee251a16d13f6fe853795b0d721ec3
devopsprasanna919 <devopsprasanna919@gmail.com>
2025-07-24 01:06:01 +0530

    Update BuildCalculator.groovy

--- src/com/academy/build/BuildCalculator.groovy
+++ src/com/academy/build/BuildCalculator.groovy
@@ -1,5 +1,7 @@
 pacakge com.academy.build;
 class BuildCalculator {
+    static class Calculator {
+    }
     def jenkins
 BuildCalculator(jenkins) {
     this.jenkins = jenkins
@@ -8,4 +10,3 @@ def add(firstNumber, secondNumber) {
     return firstNumber + secondNumber
 }
 }
-
